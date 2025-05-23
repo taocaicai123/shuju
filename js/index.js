0248dcf7f44fc03fc1be95e83fe35ea91c65d133
taocaicai <3127648985@qq.com>
2025-05-23 10:39:17 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,98 @@
+//主要几个购销地的购量占总销量的比例
+(function(){
+    //1.实例化对象
+    var myChart=echarts.init(document.querySelector('.zuo .gouwu .chart-container'));
+
+
+    //2.指定配置项和数据
+
+    var option = {
+        legend: {
+          top: 'bottom'
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        series: [
+          {
+            name: 'Nightingale Chart',
+            type: 'pie',
+            radius: [30, 140],
+            center: ['50%', '50%'],
+            roseType: 'area',
+            itemStyle: {
+              borderRadius: 8
+            },
+            data: [
+              { value: 40, name: 'rose 1' },
+              { value: 38, name: 'rose 2' },
+              { value: 32, name: 'rose 3' },
+              { value: 30, name: 'rose 4' },
+              { value: 28, name: 'rose 5' },
+              { value: 26, name: 'rose 6' },
+              { value: 22, name: 'rose 7' },
+              { value: 18, name: 'rose 8' }
+            ]
+          }
+        ]
+      };
+
+    //3.配置项和数据给我们的实例化对象
+    myChart.setOption(option);
+
+})();
+
+//近几年几月的销售量
+(function(){
+  //1.实例化对象
+  var myChart=echarts.init(document.querySelector('.you .xiaosou .chart-container'));
+
+
+  //2.指定配置项和数据
+
+  var option = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisTick: {
+          alignWithLabel: true
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: 'Direct',
+        type: 'bar',
+        barWidth: '60%',
+        data: [10, 52, 200, 334, 390, 330, 220]
+      }
+    ]
+  };
+  //3.配置项和数据给我们的实例化对象
+  myChart.setOption(option);
+
+})();
